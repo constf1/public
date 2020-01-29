b6106a0d8cf491d1ee463ccd8632878797e757d5
mico <mi-const@yandex.ru>
2020-01-29 16:37:16 +0300

    Nested routing example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from "react-router-dom";
 
 // import logo from './logo.svg';
 import './App.css';
@@ -14,6 +14,45 @@ function About() {
 
 function Users() {
   return <h2>Users</h2>;
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return (
+  <h3>Request topics ID: {topicId}</h3>
+  );
+}
+
+function Topics() {
+  let match = useRouteMatch();
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={match.url + '/components'}>Components</Link>
+        </li>
+        <li>
+          <Link to={match.url + '/props-v-state'}>Props v. State</Link>
+        </li>
+      </ul>
+
+      {
+      /** The Topics has its own <Switch> with more routes that build on the /topics URL path.
+       * You can think of the secons <Route> here as an "index" page for all topics, or the page
+       * that is shown when no topic is selected.
+       */
+      }
+      <Switch>
+        <Route path={match.path + '/:topicId'}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 const App: React.FC = () => {
@@ -31,6 +70,9 @@ const App: React.FC = () => {
             <li>
               <Link to="/users">Users</Link>
             </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
           </ul>
         </nav>
       </div>
@@ -46,6 +88,9 @@ const App: React.FC = () => {
         </Route>
         <Route path="/users">
           <Users />
+        </Route>
+        <Route path="/topics">
+          <Topics />
         </Route>
         <Route path="/">
           <Home />
